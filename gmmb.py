8c15da94c4e6aed0187ab22e7597f3a46a38a314
Denys Chernyshov <den.blackshov@gmail.com>
2021-11-09 21:04:29 +0100

    add add all

--- gmmb.py
+++ gmmb.py
@@ -1,8 +1,33 @@
 import telebot
+import uuid
 
 bot = telebot.TeleBot("2106720826:AAET8-1Lgo8Ou_Iq7E8DvqWb-Azz3QgDnhs", threaded=False)
 bot.send_message(283382228, "Starting GMMB")
 
+
+class Reminder:
+    def __init__(self, description, date, users) -> None:
+        self.users = users
+        self.id = uuid.uuid4() 
+        self.description = description
+        self.period = None
+        self.date = date
+
+    def prettify(self):
+        return f"{self.description}\n{self.prettify_users(self.users)}\n{self.date}"
+
+    def prettify_users(self, users):
+        pretty = ""
+        for user in users:
+            pretty += user.tag
+class User:
+    def __init__(self, tag) -> None:
+        self.tag = tag;
+        self.done = False
+
+reminders = {
+    # group: reminder
+}
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
@@ -17,11 +42,42 @@ def start(message):
         bot.send_message(message.chat.id, e)
         log(str(e))
 
+@bot.message_handler(commands=['add'])
+def add(message):
+    try:
+        args = extract(message)
+        description, participants, date = args.split("\n")
+        reminders[message.chat.id] = Reminder(description ,date, list([User(tag) for tag in participants.split()]))
 
+        bot.send_message(message.chat.id, "Reminder created")
+    except Exception as e:
+        bot.send_message(message.chat.id, e)
+        log(str(e))
+
+@bot.message_handler(commands=['all'])
+def add(message):
+    try:
+        all = ""
+        if message.chat.id not in reminders: 
+            bot.send_message(message.chat.id, "No reminders added.")
+            return
+
+        for rem in reminders[message.chat.id]:
+            all += rem.prettify()
+        bot.send_message(message.chat.id, all)
+    except Exception as e:
+        bot.send_message(message.chat.id, e)
+        log(str(e))
 
 """ DEBUG """
 def log(text):
     bot.send_message(283382228, text=f"{text}")
 
+def extract(command, message):
+    split = message.text.split("/"+command)
+    if not split[1] or split[1].isspace():
+        bot.send_message(message.chat.id, "Wrong format for "+command)
+        return
+    return split[1]
 
 bot.polling()
